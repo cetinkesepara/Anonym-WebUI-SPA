ba55c07e3690db788d141b74a81c9c5ed65917ae
Çetin <cetinkesepara@gmail.com>
2020-08-28 14:06:57 +0300

    Create Nav Component

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { faUser, faHome, faPenAlt, faBell } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent implements OnInit {
+
+  constructor() { }
+
+  faUser = faUser;
+  faHome = faHome;
+  faPenAlt = faPenAlt;
+  faBell = faBell;
+
+  ngOnInit() {
+  }
+
+}
